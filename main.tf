da11c305afa7482d2beb6594f4a36ad38fcd7569
Zachary Maillard <zmaillard@healthwise.org>
2020-09-02 11:07:24 -0600

    Un-necessary rg name

--- main.tf
+++ main.tf
@@ -58,13 +58,11 @@ resource "azurerm_storage_account" "tablestorage" {
 
 resource "azurerm_storage_table" "feature_flag" {
   name                 = "featureflag"
-  resource_group_name  = azurerm_resource_group.rg.name
   storage_account_name = azurerm_storage_account.tablestorage.name
 }
 
 resource "azurerm_storage_table" "feedback" {
   name                 = "feedback"
-  resource_group_name  = azurerm_resource_group.rg.name
   storage_account_name = azurerm_storage_account.tablestorage.name
 }
 
